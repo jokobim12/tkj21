414670446ae13ce45dae4ae98e7973c47a0b493c
jokobim12 <jokobimantaro88@gmail.com>
2025-03-30 13:55:37 +0700

    index

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+// navbar
 const navSlide = () =>{
     const burger = document.querySelector(".burger")
     const navLists = document.querySelector("nav")
@@ -8,3 +9,19 @@ const navSlide = () =>{
 };
 
 navSlide();
+
+// informasi
+document.querySelectorAll("details").forEach((detail) => {
+    detail.addEventListener("toggle", function () {
+      let content = this.querySelector("p"); // Ambil elemen <p> di dalam <details>
+  
+      if (this.open) {
+        let height = content.scrollHeight; // Dapatkan tinggi asli elemen
+        content.style.maxHeight = height + "px"; // Set max-height agar smooth saat dibuka
+        content.style.opacity = "1";
+      } else {
+        content.style.maxHeight = "0"; // Set max-height 0 agar smooth saat ditutup
+        content.style.opacity = "0";
+      }
+    });
+  });
