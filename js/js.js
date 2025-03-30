c33b7a26232c91749100ede25da74a485925dc06
jokobim12 <jokobimantaro88@gmail.com>
2025-03-30 09:15:55 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,10 @@
+const navSlide = () =>{
+    const burger = document.querySelector(".burger")
+    const navLists = document.querySelector("nav")
+    burger.addEventListener("click",() => {
+        navLists.classList.toggle("nav-active")
+        burger.classList.toggle("toogle-burger")
+    });
+};
+
+navSlide();
